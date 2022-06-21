068f75d84eb45ccc40f217d05a4ae3d12ff5875f
Daniel Amthauer <daniel.amthauer@gmail.com>
2022-06-21 13:40:38 -0400

    Updated CoroutineTest

--- Source/ACETeam_CoroutinesTest/Private/CoroutineTest.cpp
+++ Source/ACETeam_CoroutinesTest/Private/CoroutineTest.cpp
@@ -1,61 +1,70 @@
 // Copyright ACE Team Software S.A. All Rights Reserved.
 
-
 #include "CoroutineTest.h"
 
 #include "CoroutineElements.h"
+#include "CoroutinesSubsystem.h"
 
 IMPLEMENT_MODULE(FDefaultModuleImpl, ACETeam_CoroutinesTest)
 
 // Sets default values
 ACoroutineTest::ACoroutineTest()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 PRAGMA_DISABLE_OPTIMIZATION
+using namespace ACETeam_Coroutines;
+FCoroutineNodePtr _CoroutineTest(UWorld* World, FString TextToLog)
+{
+    //Shared ptr values can be used to share data between different execution branches,
+    // or different steps in your coroutine
+    //This way they're guaranteed to share the same lifetime as the code that's using them
+    auto SharedValue = MakeShared<int>(0);
+    //_Seq concatenates coroutine elements in a sequence. It runs one after the other until
+    // one of them fails
+    //If one of them finishes during a frame, the next in the sequence will be evaluated
+    // in the same frame
+    return _Seq(
+        //_Race evaluates each of its contained elements once per frame, in the declared order, until
+        // one finishes its execution (successfully or unsuccessfully)
+        _Race(
+            //a _Loop will run its contained element once per frame, until it fails or is aborted
+            _Loop(
+                //_Weak creates an element that will only evaluate its lambda if the passed in
+                // object is valid on evaluation
+                _Weak(World, [=] { DrawDebugPoint(World, FVector::ZeroVector, 10, FColor::White); })
+            ),
+            //_LoopSeq is a shortcut for a _Loop containing a _Seq
+            _LoopSeq(
+                //_Wait will pause this execution branch for the specified time
+                _Wait(1.0f),
+                [=] {
+                    UE_LOG(LogTemp, Log, TEXT("This text will print once per second"));
+                    UE_LOG(LogTemp, Log, TEXT("This parameter was captured by value %s"), *TextToLog);
+                },
+                //mutable lambdas can be used to have local variables that share their lifetime
+                [Counter = 0] () mutable {
+                    //When this returns false, the containing loop will finish
+                    return ++Counter <= 10;
+                },
+                [=] {
+                    //If one execution branch wants to pass data to a different
+                    //branch, you can use shared variables
+                    *SharedValue += FMath::RandHelper(10);
+                }
+            )
+        ),
+        [=] { 
+            UE_LOG(LogTemp, Log, TEXT("This log will appear after the race above finishes"));
+            UE_LOG(LogTemp, Log, TEXT("The final value stored in SharedValue is  %d"), *SharedValue); 
+        }
+    );
+}
 
-// Called when the game starts or when spawned
 void ACoroutineTest::BeginPlay()
 {
 	Super::BeginPlay();
-	using namespace ACETeam_Coroutines;
-	Executor.EnqueueCoroutine
-	(
-		_Race
-		(
-			_Scope ([]{ UE_LOG(LogTemp, Log, TEXT("Loop done")); })
-			(
-				_Loop
-				(
-					_Seq
-					(
-						[Diff = 0.0f]() mutable
-						{
-							Diff += 0.05f;
-							UE_LOG(LogTemp, Log, TEXT("Waiting for %.2f"), 1.0f - Diff);
-							return _Wait(1.0f - Diff);
-						},
-						[=, Counter = 0]() mutable 
-						{
-							UE_LOG(LogTemp, Log, TEXT("Hi %s"), *GetName());
-							if (++Counter > 5)
-								return false;
-							return true;
-						}
-					)
-				)
-			),
-			_Wait(10.0f)
-		)
-	);
-}
 
-// Called every frame
-void ACoroutineTest::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-	Executor.Step(DeltaTime);
+    UCoroutinesSubsystem::Get().StartCoroutine(_CoroutineTest(GetWorld(), TEXT("test string")));
 }
--- Source/ACETeam_CoroutinesTest/Private/CoroutineTest.h
+++ Source/ACETeam_CoroutinesTest/Private/CoroutineTest.h
@@ -5,16 +5,12 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 
-#include "CoroutineExecutor.h"
-
 #include "CoroutineTest.generated.h"
 
 UCLASS()
 class ACoroutineTest : public AActor
 {
 	GENERATED_BODY()
-
-	ACETeam_Coroutines::FCoroutineExecutor Executor;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -23,9 +19,4 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 };
